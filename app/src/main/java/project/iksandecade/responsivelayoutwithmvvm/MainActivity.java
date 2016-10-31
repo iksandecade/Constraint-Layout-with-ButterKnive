681b88d1226c865fbb42ecda8fd2c48035717ffd
iksandecade <iksandecade@gmail.com>
2016-10-31 10:58:37 +0700

    add butterKnive

--- app/src/main/java/project/iksandecade/responsivelayoutwithmvvm/MainActivity.java
+++ app/src/main/java/project/iksandecade/responsivelayoutwithmvvm/MainActivity.java
@@ -2,12 +2,36 @@ package project.iksandecade.responsivelayoutwithmvvm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tvHelloWorld)
+    TextView tvHelloWorld;
+    @BindView(R.id.btnChange)
+    Button btnChange;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        btnChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvHelloWorld.setText("Hello guys :D");
+            }
+        });
     }
 }
