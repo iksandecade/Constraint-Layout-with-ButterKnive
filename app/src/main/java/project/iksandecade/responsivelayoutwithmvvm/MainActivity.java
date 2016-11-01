deb2cf7b3edb836cc235d4098c51482ac76c041b
iksandecade <iksandecade@gmail.com>
2016-11-01 11:43:36 +0700

    add some function :D

--- app/src/main/java/project/iksandecade/responsivelayoutwithmvvm/MainActivity.java
+++ app/src/main/java/project/iksandecade/responsivelayoutwithmvvm/MainActivity.java
@@ -1,18 +1,21 @@
 package project.iksandecade.responsivelayoutwithmvvm;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 
 public class MainActivity extends AppCompatActivity {
 
-
+    @BindView(R.id.editTextName)
+    EditText etName;
+    @BindView(R.id.editTextPassword)
+    EditText etPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +24,26 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-
+    @OnClick(R.id.buttonSubmit)
+    void clickSubmit() {
+        if (isValid()){
+            Toast.makeText(this, "Congrats Bro!!!", Toast.LENGTH_SHORT).show();
+        }
     }
+
+
+    private boolean isValid() {
+        boolean status = true;
+        if (etName.getText().toString().trim().equals("")) {
+            etName.setError("Please insert name");
+            status = false;
+        }
+        if (etPassword.getText().toString().trim().equals("")) {
+            etPassword.setError("Please insert password");
+            status = false;
+        }
+
+        return status;
+    }
+
 }
